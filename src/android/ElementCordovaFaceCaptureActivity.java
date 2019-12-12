bbab15a31656ee9c4dd795d3a041f9d2f9f3cdb1
harvelouis <40568700+harvelouis@users.noreply.github.com>
2019-12-12 11:06:35 +0100

    Update ElementCordovaFaceCaptureActivity.java

--- src/android/ElementCordovaFaceCaptureActivity.java
+++ src/android/ElementCordovaFaceCaptureActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Base64;
 
 import com.element.plugin.ElementFaceMatchingSDK;
 import com.element.plugin.ElementApiTask;
